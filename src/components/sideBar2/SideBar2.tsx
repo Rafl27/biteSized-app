0bc5d6351a8b5053a9af6ade580551fb90f6502c
Rafl27 <raflsouza27@gmail.com>
2023-04-21 23:13:34 -0300

    icons added

--- src/components/sideBar2/SideBar2.tsx
+++ src/components/sideBar2/SideBar2.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Nav } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { AiFillDashboard } from 'react-icons/ai'
+import { NavItem, NavLink } from 'react-bootstrap'
+import './styles.css'
+import { IconType } from 'react-icons'
 
 interface SidebarProps {
   items: SidebarItem[]
@@ -8,22 +13,24 @@ interface SidebarProps {
 
 interface SidebarItem {
   name: string
-  icon: string
+  icon: IconType
   link: string
 }
 
 const Sidebar2: React.FC<SidebarProps> = ({ items }) => {
   return (
-    <Nav className="flex-column">
-      {items.map((item, index) => (
-        <Nav.Item key={index}>
-          <Link to={item.link} className="nav-link">
-            <i className={`material-icons ${item.icon}`}></i>
-            {item.name}
-          </Link>
-        </Nav.Item>
-      ))}
-    </Nav>
+    <div className="sidebar">
+      <Nav className="flex-column">
+        {items.map((item, index) => (
+          <Nav.Item key={index}>
+            <Nav.Link as={Link} to={item.link}>
+              {React.createElement(item.icon)}
+              {item.name}
+            </Nav.Link>
+          </Nav.Item>
+        ))}
+      </Nav>
+    </div>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,27 +2,21 @@ import React from 'react'
 import StoryCard from '../components/Stories/StoryCard'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Sidebar2 from '../components/sideBar2/SideBar2'
+import { AiFillHome } from 'react-icons/ai'
+import { BiTrendingUp } from 'react-icons/bi'
 
 // background: 'linear-gradient(to right, #f8b195, #f67280)',
 
 const items = [
-  { name: 'Home', icon: 'home', link: '/' },
-  { name: 'About', icon: 'info', link: '/about' },
-  { name: 'Contact', icon: 'email', link: '/contact' },
+  { name: 'BiteSized', icon: AiFillHome, link: '/' },
+  { name: 'Profile', icon: BiTrendingUp, link: '/about' },
+  { name: 'Trending', icon: 'email', link: '/contact' },
 ]
 
 const Home: React.FC = () => {
   return (
     <>
-      {/* <SideBar /> */}
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-3">
-            <Sidebar2 items={items} />
-          </div>
-          <div className="col-md-9">{/* Content */}</div>
-        </div>
-      </div>
+      <Sidebar2 items={items} />
       <StoryCard />
     </>
   )
