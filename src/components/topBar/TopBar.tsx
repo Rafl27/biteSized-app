8af96310c8a70d76fe6e4983d6eece0180d375e6
Rafl27 <raflsouza27@gmail.com>
2023-05-20 17:07:20 -0300

    links added / styles removed, top bar added on new story

--- src/components/topBar/TopBar.tsx
+++ src/components/topBar/TopBar.tsx
@@ -4,33 +4,57 @@ import { MdCookie } from 'react-icons/md'
 import { RiLogoutBoxRLine } from 'react-icons/ri'
 import { BsPlusCircleFill } from 'react-icons/bs'
 import { BiTrendingUp } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const TopBar: React.FC = () => {
   const userPicture = localStorage.getItem('profilePicture')
   const userName = localStorage.getItem('name')
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    localStorage.removeItem('name')
+    localStorage.removeItem('email')
+    localStorage.removeItem('profilePicture')
+    localStorage.removeItem('token')
+    navigate('/auth')
+  }
 
   return (
     <div className="top-bar">
-      <div className="logo">
-        <MdCookie className="cookie-icon" />
-        BiteSized
-      </div>
-      <div className="options">
-        {userPicture && (
-          <div className="option">
-            <img className="ppTop" src={userPicture} alt="Profile" />
-            {userName}
-          </div>
-        )}
-        <div className="option">
-          <BsPlusCircleFill className="BsPlusCircleFill" />
+      <Link
+        style={{ textDecoration: 'none', color: 'black' }}
+        to="/home"
+        className="link-home"
+      >
+        <div className="logo">
+          <MdCookie className="cookie-icon" />
+          BiteSized
         </div>
+      </Link>
+      <div className="options">
+        <Link style={{ textDecoration: 'none', color: 'black' }} to="/profile">
+          {userPicture && (
+            <div className="option">
+              <img className="ppTop" src={userPicture} alt="Profile" />
+              {userName}
+            </div>
+          )}
+        </Link>
+        <Link style={{ textDecoration: 'none', color: 'black' }} to="/create">
+          <div className="option">
+            <BsPlusCircleFill className="BsPlusCircleFill" />
+          </div>
+        </Link>
         <div className="option">
           <BiTrendingUp className="BiTrendingUp" />
         </div>
 
         <div className="option">
-          <RiLogoutBoxRLine className="RiLogoutBoxRLine" />
+          <RiLogoutBoxRLine
+            className="RiLogoutBoxRLine"
+            onClick={handleLogout}
+          />
         </div>
       </div>
     </div>
--- src/pages/createStory/CreateStory.tsx
+++ src/pages/createStory/CreateStory.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './CreateStory.css'
 import axios from 'axios'
 import { Modal } from 'react-bootstrap'
+import TopBar from '../../components/topBar/TopBar'
 
 function App() {
   //TODO: now the button is always green because useState has values.
@@ -54,80 +55,83 @@ function App() {
     }
   }
   return (
-    <div className="container">
-      <form className="createStory-form" onSubmit={handleCreateStory}>
-        <div className="inputs">
-          <input
-            type="text"
-            placeholder="Story Title"
-            name="name"
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Image URL"
-            name="img"
-            onChange={(e) => setImage(e.target.value)}
-          />
-          <textarea
-            placeholder="First Paragraph"
-            name="text"
-            onChange={(e) => setText(e.target.value)}
-          />
-          <button
-            id="createButton"
-            disabled={isCreateButtonDisabled}
-            type="submit"
-            title={isCreateButtonDisabled ? getCreateButtonTooltip() : ''}
-            className={
-              isCreateButtonDisabled ? 'disabled-button' : 'enabled-button'
-            }
-          >
-            Create
-          </button>
+    <>
+      <TopBar />
+      <div className="container">
+        <form className="createStory-form" onSubmit={handleCreateStory}>
+          <div className="inputs">
+            <input
+              type="text"
+              placeholder="Story Title"
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Image URL"
+              name="img"
+              onChange={(e) => setImage(e.target.value)}
+            />
+            <textarea
+              placeholder="First Paragraph"
+              name="text"
+              onChange={(e) => setText(e.target.value)}
+            />
+            <button
+              id="createButton"
+              disabled={isCreateButtonDisabled}
+              type="submit"
+              title={isCreateButtonDisabled ? getCreateButtonTooltip() : ''}
+              className={
+                isCreateButtonDisabled ? 'disabled-button' : 'enabled-button'
+              }
+            >
+              Create
+            </button>
+          </div>
+        </form>
+
+        <Modal show={showModal} onHide={() => setShowModal(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Story Posted!</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>{createdStoryName} has been posted.</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <a
+              href={`http://localhost:3000/story/${createdStoryName}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn btn-primary"
+            >
+              View Story
+            </a>
+            <button
+              onClick={() => setShowModal(false)}
+              className="btn btn-secondary"
+            >
+              Close
+            </button>
+          </Modal.Footer>
+        </Modal>
+
+        <div className="output">
+          {/* && is used for when a certain variable is available, if not it won't be rendered. */}
+          {name && <h1>{name}</h1>}
+          {image && (
+            <div className="image-wrapper">
+              <img id="createImage" src={image} alt="User input" />
+            </div>
+          )}
+          {text && (
+            <div className="paragraph-wrapper">
+              <h5 id="paragraph">{text}</h5>
+            </div>
+          )}
         </div>
-      </form>
-
-      <Modal show={showModal} onHide={() => setShowModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Story Posted!</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>{createdStoryName} has been posted.</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <a
-            href={`http://localhost:3000/story/${createdStoryName}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="btn btn-primary"
-          >
-            View Story
-          </a>
-          <button
-            onClick={() => setShowModal(false)}
-            className="btn btn-secondary"
-          >
-            Close
-          </button>
-        </Modal.Footer>
-      </Modal>
-
-      <div className="output">
-        {/* && is used for when a certain variable is available, if not it won't be rendered. */}
-        {name && <h1>{name}</h1>}
-        {image && (
-          <div className="image-wrapper">
-            <img id="createImage" src={image} alt="User input" />
-          </div>
-        )}
-        {text && (
-          <div className="paragraph-wrapper">
-            <h5 id="paragraph">{text}</h5>
-          </div>
-        )}
       </div>
-    </div>
+    </>
   )
 }
 
