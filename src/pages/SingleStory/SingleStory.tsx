c492f36776e8c7db6b88fbb5bed18e5d38911921
Rafl27 <raflsouza27@gmail.com>
2023-05-28 20:17:31 -0300

    singleStory styles

--- src/pages/SingleStory/SingleStory.tsx
+++ src/pages/SingleStory/SingleStory.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
+import './SingleStory.css'
 
 interface Story {
   _id: string
@@ -19,7 +20,7 @@ interface Comment {
   _id: string
   user: {
     _id: string
-    username: string
+    name: string
   }
   text: string
   replies: Comment[]
@@ -54,27 +55,33 @@ const SingleStory = () => {
   }, [_id])
 
   return (
-    <div>
-      {story.img && <img src={story.img} alt={story.name} />}
-      <h1>{story.name}</h1>
-      <p>{story.text}</p>
-      <p>Written by: {story.user.name}</p>
-      <p>Published on: {new Date(story.date).toLocaleDateString()}</p>
-      <h2>Comments:</h2>
-      {story.comments &&
-        story.comments.map((comment) => (
-          <div key={comment._id}>
-            <p>{comment.text}</p>
-            <p>Commented by: {comment.user && comment.user.username}</p>
-            {comment.replies &&
-              comment.replies.map((reply) => (
-                <div key={reply._id}>
-                  <p>{reply.text}</p>
-                  <p>Replied by: {reply.user && reply.user.username}</p>
-                </div>
-              ))}
-          </div>
-        ))}
+    <div className="single-story-container">
+      <div className="header">
+        <h1>{story.name}</h1>
+        {story.img && <img src={story.img} alt={story.name} />}
+      </div>
+      <div className="content">
+        <p>{story.text}</p>
+        <p>Written by: {story.user.name}</p>
+        <p>Published on: {new Date(story.date).toLocaleDateString()}</p>
+      </div>
+      <div className="comments">
+        <h2>Comments:</h2>
+        {story.comments &&
+          story.comments.map((comment) => (
+            <div key={comment._id} className="comment">
+              <p>{comment.text}</p>
+              <p>Commented by: {}</p>
+              {comment.replies &&
+                comment.replies.map((reply) => (
+                  <div key={reply._id} className="reply">
+                    <p>{reply.text}</p>
+                    <p>Replied by: {reply.user && reply.user.name}</p>
+                  </div>
+                ))}
+            </div>
+          ))}
+      </div>
     </div>
   )
 }
