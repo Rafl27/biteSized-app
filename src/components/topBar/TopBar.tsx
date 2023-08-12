5273761610c1bc81facc5b5263d956d11dceb4cc
Rafl27 <raflsouza27@gmail.com>
2023-08-12 16:07:08 -0300

    top bar working with new api.

--- src/components/topBar/TopBar.tsx
+++ src/components/topBar/TopBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './TopBar.css'
 import { MdCookie } from 'react-icons/md'
 import { RiLogoutBoxRLine } from 'react-icons/ri'
@@ -6,11 +6,13 @@ import { BsPlusCircleFill } from 'react-icons/bs'
 import { BiTrendingUp } from 'react-icons/bi'
 import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
+import axios from "axios";
 
 const TopBar: React.FC = () => {
   const userPicture = localStorage.getItem('profilePicture')
   const userName = localStorage.getItem('name')
   const navigate = useNavigate()
+  const token = localStorage.getItem('token');
 
   const handleLogout = () => {
     localStorage.removeItem('name')
@@ -19,6 +21,34 @@ const TopBar: React.FC = () => {
     localStorage.removeItem('token')
     navigate('/auth')
   }
+
+  const [userData, setUserData] = useState({
+    username: '',
+    email: '',
+    profilePicture: ''
+  });
+
+  useEffect(() => {
+    if (token) {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      };
+
+      // Fetch user data from the API with the token
+      axios.get('http://localhost:8080/user/info', config)
+          .then(response => {
+            setUserData(response.data);
+          })
+          .catch(error => {
+            // Handle error if the API request fails
+            console.error('Error fetching user data:', error);
+          });
+    }
+  }, [token]);
+
+  console.log(userData)
 
   return (
     <div className="top-bar">
@@ -36,8 +66,8 @@ const TopBar: React.FC = () => {
         <Link style={{ textDecoration: 'none', color: 'black' }} to="/profile">
           {userPicture && (
             <div className="option">
-              <img className="ppTop" src={userPicture} alt="Profile" />
-              {userName}
+              <img className="ppTop" src={userData.profilePicture} alt="Profile" />
+              {userData.username}
             </div>
           )}
         </Link>
