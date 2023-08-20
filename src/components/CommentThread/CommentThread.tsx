af06b0b69adf2f3df624e11b91fe0b2637c215ab
Rafl27 <raflsouza27@gmail.com>
2023-08-20 11:48:14 -0300

    reply button

--- src/components/CommentThread/CommentThread.tsx
+++ src/components/CommentThread/CommentThread.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import './CommentThread.css'
+import {RiChat3Line} from "react-icons/ri";
 
 const CommentThread = ({ comment }) => {
     return (
@@ -6,12 +8,16 @@ const CommentThread = ({ comment }) => {
             <div className="userInfo">
                 <img src={comment.userProfilePic} alt="User Profile" />
                 <span>{comment.userUsername}</span>
-                <span>{comment.dateComment}</span>
             </div>
-            <img src={comment.artComment} alt="Comment Art" />
+            {comment.artComment !== "" && (
+                <img className="commentArt" src={comment.artComment} alt="Comment Art" />
+            )}
             <div className="comment-content">{comment.contentComment}</div>
+            <button className="replyButton">
+            <RiChat3Line className="chatIcon" />
+               Reply
+              </button>
 
-            {/* Render replies */}
             {comment.replies && comment.replies.length > 0 && (
                 <div className="replies">
                     {comment.replies.map(reply => (
