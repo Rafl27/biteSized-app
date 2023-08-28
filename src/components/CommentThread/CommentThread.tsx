b10c814426aaa691ba8f1ded2e74d69e9021087c
Rafl27 <raflsouza27@gmail.com>
2023-08-28 19:34:37 -0300

    handle comment upvotes.

--- src/components/CommentThread/CommentThread.tsx
+++ src/components/CommentThread/CommentThread.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import './CommentThread.css'
 import {RiChat3Line} from "react-icons/ri";
 import {ImArrowDown, ImArrowUp} from "react-icons/im";
+import {upvoteStory, upvoteComment} from "../../services/api";
 
 const CommentThread = ({ comment }) => {
+    const token : string = localStorage.getItem('token')
+    const handleUpvote = async (commentId: number) => {
+        try {
+            const updatedComment = await upvoteComment(commentId, token);
+            comment.upvotes = updatedComment.upvote
+        } catch (err) {
+            console.error(err);
+        }
+    }
     return (
         <div className="comment">
             <div className="userInfo">
@@ -14,15 +24,14 @@ const CommentThread = ({ comment }) => {
                 <img className="commentArt" src={comment.artComment} alt="Comment Art" />
             )}
             <div className="comment-content">{comment.contentComment}</div>
-            {/*TODO: make the request to upvote and downvote comments, and also do the cool thing with variables to use the same function*/}
             <div className="button-container">
                 <button className="replyButton">
                     <RiChat3Line className="chatIcon" />
                     Reply
                 </button>
                 <div className="vote-container">
-                    <button className='vote-button upvote'> <ImArrowUp /> <p>opa</p> </button>
-                    <button className='vote-button downvote'> <ImArrowDown /> <p>opa</p> </button>
+                    <button className='vote-button upvote' onClick={() => handleUpvote(comment.idComment)}> <ImArrowUp /> <p>{comment.upvotesComment}</p> </button>
+                    <button className='vote-button downvote'> <ImArrowDown /> <p>{comment.downvotesComment}</p> </button>
                 </div>
             </div>
             {comment.replies && comment.replies.length > 0 && (
