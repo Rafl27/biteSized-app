e13972b3a035cd4be3dae47ef60e5bf4825f2cd8
Rafl27 <raflsouza27@gmail.com>
2023-10-19 21:51:30 -0300

    New story layout bug fixed.

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const NotFoundPage: React.FC = () => {
   return (
-    <div className="container">
+    <div className="container-404">
         <MdCookie className="cookie-icon404" />
       <h1 className="heading">404 Not Found</h1>
       <p className="paragraph">Sorry, the page you are looking for does not exist.</p>
