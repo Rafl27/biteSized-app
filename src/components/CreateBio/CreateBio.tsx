12bfea28536f5f80a4067d0705010b9c97c068f1
Rafl27 <raflsouza27@gmail.com>
2023-11-02 18:38:36 -0300

    create bio component.

---
+++ src/components/CreateBio/CreateBio.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './CreateBio.css';
+import Modal from '../Modal/Modal'
+
+const CreateBio = () => {
+    const [modalOpen, setModalOpen] = useState(false);
+    const [userBio, setUserBio] = useState('');
+
+    const handleOpenModal = () => {
+        setModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setModalOpen(false);
+    };
+
+    const handleSaveBio = (bio: string) => {
+        setUserBio(bio);
+        setModalOpen(false);
+    };
+
+    return (
+        <div>
+            <button onClick={handleOpenModal}>Create User Bio</button>
+            {modalOpen && (
+                <Modal
+                    onClose={handleCloseModal}
+                    onSave={handleSaveBio}
+                    initialText={userBio}
+                    modalTitle={'User bio'}
+                />
+            )}
+        </div>
+    );
+};
+
+export default CreateBio;
