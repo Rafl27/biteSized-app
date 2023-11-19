6ae7e390330044b95d2d7585d17a04a48ed2c966
Rafl27 <raflsouza27@gmail.com>
2023-11-19 19:00:41 -0300

    user votes interface.

---
+++ src/interfaces/UserVotesPar.ts
@@ -0,0 +1,10 @@
+export interface UserVotesPar {
+    art : string,
+    content : string,
+    date : string,
+    downvotes : number,
+    upvotes : number,
+    storyId : number,
+    voteType : string,
+    commentId : number
+}
