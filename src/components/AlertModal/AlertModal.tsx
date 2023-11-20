e965676f26ddb034c5bdce79b597ad0371bf664a
Rafl27 <raflsouza27@gmail.com>
2023-11-20 10:22:11 -0300

    alert modal.

---
+++ src/components/AlertModal/AlertModal.tsx
@@ -0,0 +1,17 @@
+import './AlertModal.css'
+
+import React from 'react';
+const AlertModal = ({message}) => {
+    return (
+        <div className='modalT'>
+            <div className='modal-content'>
+                <h2 className="modal-title">{message}</h2>
+                <div className="button-container">
+                    <button className="modal-button close-button">Close</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AlertModal;
