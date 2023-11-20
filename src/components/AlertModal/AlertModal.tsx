0035aaa27c08f5dcd5c7c62d5d37f54d33deb7bf
Rafl27 <raflsouza27@gmail.com>
2023-11-20 13:32:20 -0300

    on click event

--- src/components/AlertModal/AlertModal.tsx
+++ src/components/AlertModal/AlertModal.tsx
@@ -1,13 +1,13 @@
 import './AlertModal.css'
 
-import React from 'react';
-const AlertModal = ({message} : {message : string}) => {
+import React, {MouseEventHandler} from 'react';
+const AlertModal = ({message, onClose} : {message : string, onClose : MouseEventHandler}) => {
     return (
         <div className='modalT'>
             <div className='modal-content'>
                 <h2 className="modal-title">{message}</h2>
                 <div className="button-container">
-                    <button className="modal-button close-button">Close</button>
+                    <button className="modal-button close-button" onClick={onClose}>Close</button>
                 </div>
             </div>
         </div>
