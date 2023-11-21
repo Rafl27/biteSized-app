60285d086ca04408adb81736858fc285d8f6b8d6
Rafl27 <raflsouza27@gmail.com>
2023-11-21 11:45:20 -0300

    fetch stories and bio.

--- src/pages/VIsitProfile/VIsitProfile.tsx
+++ src/pages/VIsitProfile/VIsitProfile.tsx
@@ -5,6 +5,8 @@ import axios from "axios";
 import ProfileNavBar from "../../components/ProfileNavBar/ProfileNavBar";
 import ProfileCreateStories from "../../components/ProfileCreatedStories/ProfileCreateStories";
 import UserVotes from "../../components/UserVotes/UserVotes";
+import {Story} from "../../interfaces";
+import CreateBio from "../../components/CreateBio/CreateBio";
 
 const VIsitProfile = () => {
 
@@ -26,9 +28,41 @@ const VIsitProfile = () => {
             axios.get(`${import.meta.env.VITE_API_BASE_URL}/user/${userId}/info-visit-profile`)
                 .then((response) => {
                     setUserData(response.data)
+                    axios
+                        .get(`${import.meta.env.VITE_API_BASE_URL}/user/bio/${response.data.id}`)
+                        .then((bioResponse) => {
+                            setUserBio(bioResponse.data);
+                        })
+                        .catch((error) => {
+                            console.error('Error fetching user bio', error);
+                        });
                 })
         }
     }, [userId]);
+
+    useEffect(() => {
+        fetchStories()
+    }, [])
+
+
+    const [stories, setStories] = useState<Story[]>([])
+
+    const fetchStories = async () => {
+        try {
+            const response = await fetch (
+                `${import.meta.env.VITE_API_BASE_URL}/story/${userId}/visit`
+            )
+            if (!response.ok){
+                throw new Error("Failed to fetch stories")
+            }
+            const storiesData = await response.json()
+            setStories(storiesData)
+        }catch (error) {
+            console.log('Error fetching stories', error)
+        }
+    }
+
+
 
     const [activeOption, setActiveOption] = useState('stories');
 
@@ -40,8 +74,11 @@ const VIsitProfile = () => {
                     <img src={userData.profilePicture} alt="Profile" />
                 </div>
                 <h2>{userData.username}</h2>
+                {userBio.bio &&
+                    <p className="bio">{userBio.bio}</p>
+                }
                 <ProfileNavBar setActiveOption={setActiveOption} />
-                {/*{activeOption === 'stories' && <ProfileCreateStories stories={stories} />}*/}
+                {activeOption === 'stories' && <ProfileCreateStories stories={stories} />}
                 {activeOption === 'votes' && <UserVotes userId={Number(userData.id)} />}
             </div>
         </>
