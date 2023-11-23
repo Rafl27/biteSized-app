0ae5efc488c9927638bdfba0b977fc26efc6f07f
Rafl27 <raflsouza27@gmail.com>
2023-11-23 10:27:55 -0300

    working following count.

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -4,10 +4,11 @@ import React, {useEffect, useState} from 'react';
 import CreateBio from "../CreateBio/CreateBio";
 import {UserBio} from "../../interfaces/UserBio";
 import {UserInfoData} from "../../interfaces/UserInfoData";
-import {fetchFollowerCount} from "../../services/api";
+import {fetchFollowerCount, fetchFollowingCount} from "../../services/api";
 
 const UserInfo = ({personalPage, userInfoData, userBio, token} : {personalPage : boolean, userInfoData : UserInfoData, userBio : UserBio, token? : String}) => {
     const [followerCount, setFollowerCount] = useState<number>(0)
+    const [followingCount, setFollowingCount] = useState<number>(0)
 
     useEffect(() => {
         fetchFollowerCount(userInfoData.id)
@@ -15,6 +16,14 @@ const UserInfo = ({personalPage, userInfoData, userBio, token} : {personalPage :
                 setFollowerCount(data.following);
             })
             .catch(error => console.error("Error", error));
+    }, [userInfoData]);
+
+    useEffect(() => {
+        fetchFollowingCount(userInfoData.id)
+            .then(data => {
+                setFollowingCount(data.following)
+            })
+            .catch(error => console.error("Error", error))
     }, [userInfoData]);
 
 
@@ -37,7 +46,12 @@ const UserInfo = ({personalPage, userInfoData, userBio, token} : {personalPage :
                                 (
                                     <p><b>0</b> Followers</p>
                                 )}
-                            <p><b>33</b> Following</p>
+                            {followingCount ? (
+                                <p><b>{followingCount}</b> Following</p>
+                            ) : (
+                                <p><b>0</b> Following</p>
+                            )
+                            }
                         </div>
                         <div className="name-follow">
                             <h2 className="username">{userInfoData.username}</h2>
