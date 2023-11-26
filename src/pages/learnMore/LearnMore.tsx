80552abfe594d59871da08d32746a009a124d777
Rafl27 <raflsouza27@gmail.com>
2023-11-26 14:29:56 -0300

    text improvement.

--- src/pages/learnMore/LearnMore.tsx
+++ src/pages/learnMore/LearnMore.tsx
@@ -7,7 +7,8 @@ const LearnMore = () => {
         <div>
             <TopBar />
             <h1 className="titles">How does BiteSized Work?</h1>
-            <p className="paragraphs">Lets start with why is this platform called what it is, currently I dont have as much time as I had a few years ago to read epic fantasy, and I wanted a way to quickly get some of this content, in bite sizes (I just said the name of the platform ╰(*°▽°*)╯) when I started creating it, I came up with the idea, wouldnt it be awesome if people were able to create stories colaboratevily? That way you would never find a bad ending, or plot holes or anything of such kind, because you could fix it or someone else.</p>
+            <p className="paragraphs">So, you know how life gets busy and you don’t have as much time to dive into epic fantasy like you used to? That’s where this platform comes in. It’s all about getting your fantasy fix in bite-sized pieces (yep, that’s why we call it what we do!).
+                When I started building this thing, I had a lightbulb moment. What if everyone could chip in and weave stories together? No more groaning at bad endings or plot holes, 'cause you or anyone else could step in and fix it. Cool, right? That’s what we’re all about here.</p>
 
             <h3 className="titles">How do threads work?</h3>
             <p className="paragraphs">BiteSized is a platform where you can create and colaborate in stories, be it with paragraphs or your artwork, in this platform, we call replies to stories threads, the reason why? Thats because one of the premisses of the platform, creating stories with a community, means such stories can have infinite endings, so each thread can have multiple independent endings.</p>
